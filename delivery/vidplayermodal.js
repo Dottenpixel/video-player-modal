41bd083fbdb167544fb4fe410902ce30fbbb3f11
DouG Molidor <doug@creativelift.net>
2011-06-09 20:19:44 -0700

    Initial delivery files copied

---
+++ delivery/vidplayermodal.js
@@ -0,0 +1,35 @@
+var launchVideoModal = function(tTxt,vSrc,clickTxt,clickUrl){
+	$("div.videoModal").remove();
+	$("body").append('<div class="videoModal"><div id="videoModalSWF" class="swfArea"></div></div>');
+	$(".videoModal").css({
+		"width": "680px",
+		"height": "430px",
+		"background": "transparent",
+		"position": "fixed",
+	    "top": "5px",
+		"left": "50%",
+	    "margin-left": "-340px",
+		"z-index": "9999"
+	});
+	//$("div.videoModal").append('<p>' + tTxt + '</p><p>' + vSrc + '</p><p>' + clickTxt + '</p><p>' + clickUrl + '</p>');
+
+	var flashvars = { 
+		"titleTxt": tTxt, 
+		"vidSrc" : vSrc,
+		"clickTxt" : clickTxt,
+		"clickUrl" : clickUrl 
+	};
+	var params = { "wmode":"transparent", "allowScriptAccess":"sameDomain", "allowFullscreen":"true", "base":"swf" };
+	var setAttributes = function (id) {
+		return { "id":id, "class":"flash-swf-object" };
+	}
+	swfobject.embedSWF("swf/meijer_photo_vidplayer.swf", "videoModalSWF", "640", "390", "9.0.0", "swf/expressInstall.swf", flashvars, params, setAttributes("swfLPHero"));
+};
+
+var closeVideoModal = function() {
+	$("div.videoModal").remove();
+}
+	
+$(document).ready(function(){
+	//$("input.launcher").bind("click",function(){ launchVideoModal(new Date(),new Date(),new Date(),new Date()) });
+});
